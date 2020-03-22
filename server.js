2726083b86c341dee9597f5e7ba2a56e177b0d76
Khanh Mai <57655821+kmai01@users.noreply.github.com>
2020-03-22 05:15:25 -0500

    modified server file

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require("express");
+var fs = require("fs");
+var path = require("path")
 
 
 // Tells node that we are creating an "express" server
 var app = express();
 
-// Sets an initial port. We"ll use this later in our listener
+// Sets an initial port. We"ll use this later in our listener  
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
@@ -16,9 +18,15 @@ app.use(express.json());
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+
 
 // Listen on port 8080
 app.listen(PORT, function() {
