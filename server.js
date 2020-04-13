6aa451ee7c80b5f6d9044aaa35d7d8acdbec2570
unknown <kmaibox@hotmail.com>
2020-04-13 00:58:37 -0500

    change port to 3000

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var db = [];
 var app = express();
 
 // Sets an initial port. We"ll use this later in our listener  
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
